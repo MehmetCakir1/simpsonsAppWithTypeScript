a2ee00a7371f8c9a264cbe7956aaa503bd4bf41b
MehmetCakir1 <mehmetcakir4227@gmail.com>
2022-10-09 16:35:21 +0300

    details

--- src/components/Singlesimpson.tsx
+++ src/components/Singlesimpson.tsx
@@ -3,6 +3,7 @@ import {BsFillArrowUpCircleFill,BsFillArrowDownCircleFill} from "react-icons/bs"
 import {FaTrashAlt} from "react-icons/fa"
 import { useAppDispatch, useAppSelector } from '../app/hooks'
 import {removeItem,upItem,downItem} from "../features/simpsonsSlice"
+import { useNavigate } from 'react-router-dom'
 
 interface ISimpsonProps {
   item : ISimpson,
@@ -11,12 +12,17 @@ const Singlesimpson = ({item}:ISimpsonProps) => {
   const {simpsons}=useAppSelector(state=>state.simpsons)
   const {name,avatar,id}=item
   const dispatch=useAppDispatch()
+  const navigate=useNavigate()
   // console.log(index)
   return (
     <div className='flex items-center justify-between container m-auto max-w-2xl my-1 border-b-2 border-slate-300 py-2'>
       <p className='w-1/12 flex items-center justify-center text-lg'>{simpsons.indexOf(item)+1}</p>
-      <img src={avatar.split("revision")[0]} alt={name} className=" h-[90px] w-2/12 sm:w-1/12 max-w-[60px]"/>
-      <h1 className='w-5/12 sm:w-6-12 text-lg'>{name}</h1>
+      <img src={avatar.split("revision")[0]} alt={name} className=" h-[90px] w-2/12 sm:w-1/12 max-w-[60px] cursor-pointer"
+      onClick={()=>navigate(`/details/${id}`,{state:item})}
+      />
+      <h1 className='w-5/12 sm:w-6-12 text-lg cursor-pointer'
+      onClick={()=>navigate(`/details/${id}`,{state:item})}
+      >{name}</h1>
       <button className='w-1/12 flex items-center justify-center text-2xl text-cyan-500 bg-transparent'
       onClick={()=>dispatch(upItem(id))}
       ><BsFillArrowUpCircleFill/></button>
--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import { useLocation, useNavigate, useParams } from 'react-router-dom'
+import { useAppSelector } from '../app/hooks'
 
 const Detail = () => {
+  const {id}=useParams()
+  const {state}=useLocation()
+  const {simpsons}=useAppSelector(state=>state.simpsons)
+  const {loading}=useAppSelector(state=>state.simpsons)
+  const navigate=useNavigate()
+// console.log(state)
+// console.log(loading)
+const {name,avatar,job,description}=state
+console.log(avatar)
+
+
   return (
-    <div>Detail</div>
+    <> 
+     {
+      loading ?(
+        <h1 className='text-center text-3xl font-semibold mt-5'>LOADING...</h1>
+      )
+      :
+      (
+        <div className='flex flex-col items-center justify-center max-w-xl m-auto mt-9'>
+        <img src={avatar.includes("revision") ? avatar.split("revision")[0] : avatar.startsWith("https://") && avatar.endsWith(".png")|| avatar.endsWith(".jpg") ? avatar : "images/Simpson.png"} alt={name} className="h-[250px]" />
+        <p className='font-bold text-2xl py-3 text-red-800'>{name}</p>
+        <p className='text-xl py-1'>{job}</p>
+        <p className='indent-3 py-1'>{description}</p>
+        <button className='px-4 py-1 bg-blue-700 text-white' onClick={()=>navigate("/")}>Go Back</button>
+      </div>
+      )
+    }
+    </>
+  
   )
 }
 
